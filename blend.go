79bc2b6cb647c9b8082fde45f8b684d55c6d2389
Guillermo Estrada <phrzn@live.com>
2021-02-20 14:47:16 -0600

    Initial code cleaning

--- blend.go
+++ blend.go
@@ -9,18 +9,18 @@
 // the function is applied to each pixel where the top layer (src)
 // overlaps the bottom layer (dst) of both given 'image' interfaces.
 //
-// This library provides many of the widely used blending functions 
+// This library provides many of the widely used blending functions
 // to be used either as 'mode' parameter to the Blend() primary
 // function, or to be used individually providing two 'color' interfaces.
-// You can implement your own blending modes and pass them into the 
+// You can implement your own blending modes and pass them into the
 // Blend() function.
 //
 // This is the list of the currently implemented blending modes:
 //
-// Add, Color, Color Burn, Color Dodge, Darken, Darker Color, Difference, 
-// Divide, Exclusion, Hard Light, Hard Mix, Hue, Lighten, Lighter Color, 
-// Linear Burn, Linear Dodge, Linear Light, Luminosity, Multiply, Overlay, 
-// Phoenix, Pin Light, Reflex, Saturation, Screen, Soft Light, Substract, 
+// Add, Color, Color Burn, Color Dodge, Darken, Darker Color, Difference,
+// Divide, Exclusion, Hard Light, Hard Mix, Hue, Lighten, Lighter Color,
+// Linear Burn, Linear Dodge, Linear Light, Luminosity, Multiply, Overlay,
+// Phoenix, Pin Light, Reflex, Saturation, Screen, Soft Light, Substract,
 // Vivid Light.
 //
 // Check github for more details:
@@ -35,7 +35,7 @@ import (
 )
 
 // Constants of max and mid values for uint16 for internal use.
-// This can be changed to make the algorithms use uint8 instead, 
+// This can be changed to make the algorithms use uint8 instead,
 // but they are kept this way to provide more acurate calculations
 // and to support all of the color modes in the 'image' package.
 const (
@@ -43,44 +43,45 @@ const (
 	mid = max / 2.0
 )
 
-var (
-	Add          BlendFunc
-	Color        BlendFunc
-	ColorBurn    BlendFunc
-	ColorDodge   BlendFunc
-	Darken       BlendFunc
-	DarkerColor  BlendFunc
-	Difference   BlendFunc
-	Divide       BlendFunc
-	Exclusion    BlendFunc
-	HardLight    BlendFunc
-	HardMix      BlendFunc
-	Hue          BlendFunc
-	Lighten      BlendFunc
-	LighterColor BlendFunc
-	LinearBurn   BlendFunc
-	LinearDodge  BlendFunc
-	LinearLight  BlendFunc
-	Luminosity   BlendFunc
-	Multiply     BlendFunc
-	Overlay      BlendFunc
-	Phoenix      BlendFunc
-	PinLight     BlendFunc
-	Reflex       BlendFunc
-	Saturation   BlendFunc
-	Screen       BlendFunc
-	SoftLight    BlendFunc
-	Substract    BlendFunc
-	VividLight   BlendFunc
-)
-
-// A blend function or blend mode receives a destination color and 
+// BlendFunc or blend mode receives a destination color and
 // a source color, then returns a transformation of them. Blend()
-// function receives a BlendFunc and applies it to every pixel in 
+// function receives a BlendFunc and applies it to every pixel in
 // the overlaping areas of two given images.
 type BlendFunc func(dst, src color.Color) color.Color
 
-// Blends src image (top layer) into dst image (bottom layer) using
+// Available Moodes map
+var Modes = map[string]BlendFunc{
+	"add":           Add,
+	"color":         Color,
+	"color_burn":    ColorBurn,
+	"color_dodge":   ColorDodge,
+	"darken":        Darken,
+	"darker_color":  DarkerColor,
+	"difference":    Difference,
+	"divide":        Divide,
+	"exclusion":     Exclusion,
+	"hard_light":    HardLight,
+	"hard_mix":      HardMix,
+	"hue":           Hue,
+	"lighten":       Lighten,
+	"lighter_color": LighterColor,
+	"linear_burn":   LinearBurn,
+	"linear_dodge":  LinearDodge,
+	"linear_light":  LinearLight,
+	"luminosity":    Luminosity,
+	"multiply":      Multiply,
+	"overlay":       Overlay,
+	"phoenix":       Phoenix,
+	"pin_light":     PinLight,
+	"reflex":        Reflex,
+	"saturation":    Saturation,
+	"screen":        Screen,
+	"soft_light":    SoftLight,
+	"substract":     Substract,
+	"vivid_light":   VividLight,
+}
+
+// BlendImage blends src image (top layer) into dst image (bottom layer) using
 // the BlendFunc provided by mode. BlendFunc is applied to each pixel
 // where the src image overlaps the dst image and the result is stored
 // in the original dst image, src image is unmutable.
@@ -95,7 +96,7 @@ func BlendImage(dst draw.Image, src image.Image, mode BlendFunc) {
 	}
 }
 
-// Blends src image (top layer) into dst image (bottom layer) using
+// BlendNewImage blends src image (top layer) into dst image (bottom layer) using
 // the BlendFunc provided by mode. BlendFunc is applied to each pixel
 // where the src image overlaps the dst image and returns the resulting
 // image without modifying src, or dst as they are both unmutable.
@@ -128,46 +129,46 @@ func blendPerChannel(dst, src color.Color, bf func(float64, float64) float64) co
 // Blending modes supported by Photoshop in order.
 /*-------------------------------------------------------*/
 
-// DARKEN
-func darken(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, darken_per_ch)
+// Darken ...
+func Darken(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, darken)
 }
-func darken_per_ch(d, s float64) float64 {
+func darken(d, s float64) float64 {
 	return math.Min(d, s)
 }
 
-// MULTIPLY
-func multiply(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, multiply_per_ch)
+// Multiply ...
+func Multiply(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, multiply)
 }
-func multiply_per_ch(d, s float64) float64 {
+func multiply(d, s float64) float64 {
 	return s * d / max
 }
 
-// COLOR BURN
-func color_burn(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, color_burn_per_ch)
+// ColorBurn ...
+func ColorBurn(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, colorBurn)
 }
-func color_burn_per_ch(d, s float64) float64 {
+func colorBurn(d, s float64) float64 {
 	if s == 0.0 {
 		return s
 	}
 	return math.Max(0.0, max-((max-d)*max/s))
 }
 
-// LINEAR BURN
-func linear_burn(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, linear_burn_per_ch)
+// LinearBurn ...
+func LinearBurn(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, linearBurn)
 }
-func linear_burn_per_ch(d, s float64) float64 {
+func linearBurn(d, s float64) float64 {
 	if (s + d) < max {
 		return 0.0
 	}
 	return s + d - max
 }
 
-// DARKER COLOR
-func darker_color(dst, src color.Color) color.Color {
+// DarkerColor ...
+func DarkerColor(dst, src color.Color) color.Color {
 	s, d := color2rgbaf64(src), color2rgbaf64(dst)
 	if s.r+s.g+s.b > d.r+d.g+d.b {
 		return dst
@@ -175,45 +176,43 @@ func darker_color(dst, src color.Color) color.Color {
 	return src
 }
 
-/*-------------------------------------------------------*/
-
-// LIGHTEN
-func lighten(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, lighten_per_ch)
+// Lighten ...
+func Lighten(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, lighten)
 }
-func lighten_per_ch(d, s float64) float64 {
+func lighten(d, s float64) float64 {
 	return math.Max(d, s)
 }
 
-// SCREEN
-func screen(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, screen_per_ch)
+// Screen ...
+func Screen(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, screen)
 }
-func screen_per_ch(d, s float64) float64 {
+func screen(d, s float64) float64 {
 	return s + d - s*d/max
 }
 
-// COLOR DODGE
-func color_dodge(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, color_dodge_per_ch)
+// ColorDodge ...
+func ColorDodge(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, colorDodge)
 }
-func color_dodge_per_ch(d, s float64) float64 {
+func colorDodge(d, s float64) float64 {
 	if s == max {
 		return s
 	}
 	return math.Min(max, (d * max / (max - s)))
 }
 
-// LINEAR DODGE
-func linear_dodge(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, linear_dodge_per_ch)
+// LinearDodge ...
+func LinearDodge(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, linearDodge)
 }
-func linear_dodge_per_ch(d, s float64) float64 {
+func linearDodge(d, s float64) float64 {
 	return math.Min(s+d, max)
 }
 
-// LIGHTER COLOR
-func lighter_color(dst, src color.Color) color.Color {
+// LighterColor ...
+func LighterColor(dst, src color.Color) color.Color {
 	s, d := color2rgbaf64(src), color2rgbaf64(dst)
 	if s.r+s.g+s.b > d.r+d.g+d.b {
 		return src
@@ -223,75 +222,75 @@ func lighter_color(dst, src color.Color) color.Color {
 
 /*-------------------------------------------------------*/
 
-// OVERLAY
-func overlay(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, overlay_per_ch)
+// Overlay ...
+func Overlay(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, overlay)
 }
-func overlay_per_ch(d, s float64) float64 {
+func overlay(d, s float64) float64 {
 	if d < mid {
 		return 2 * s * d / max
 	}
 	return max - 2*(max-s)*(max-d)/max
 }
 
-// SOFT LIGHT
-func soft_light(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, soft_light_per_ch)
+// SoftLight ...
+func SoftLight(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, softLight)
 }
-func soft_light_per_ch(d, s float64) float64 {
+func softLight(d, s float64) float64 {
 	return (d / max) * (d + (2*s/max)*(max-d))
 }
 
-// HARD LIGHT
-func hard_light(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, hard_light_per_ch)
+// HardLight ...
+func HardLight(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, hardLight)
 }
-func hard_light_per_ch(d, s float64) float64 {
+func hardLight(d, s float64) float64 {
 	if s > mid {
 		return d + (max-d)*((s-mid)/mid)
 	}
 	return d * s / mid
 }
 
-// VIVID LIGHT (check)
-func vivid_light(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, vivid_light_per_ch)
+// VividLight ...
+func VividLight(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, vividLight)
 }
-func vivid_light_per_ch(d, s float64) float64 {
+func vividLight(d, s float64) float64 {
 	if s < mid {
-		return color_burn_per_ch((2 * s), d)
+		return colorBurn((2 * s), d)
 	}
-	return color_dodge_per_ch((2 * (s - mid)), d)
+	return colorDodge((2 * (s - mid)), d)
 }
 
-// LINEAR LIGHT
-func linear_light(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, linear_light_per_ch)
+// LinearLight ...
+func LinearLight(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, linearLight)
 }
-func linear_light_per_ch(d, s float64) float64 {
+func linearLight(d, s float64) float64 {
 	if s < mid {
-		return linear_burn_per_ch((2 * s), d)
+		return linearBurn((2 * s), d)
 	}
-	return linear_dodge_per_ch((2 * (s - mid)), d)
+	return linearDodge((2 * (s - mid)), d)
 }
 
-// PIN LIGHT
-func pin_light(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, pin_light_per_ch)
+// PinLight ...
+func PinLight(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, pinLight)
 }
-func pin_light_per_ch(d, s float64) float64 {
+func pinLight(d, s float64) float64 {
 	if s < mid {
-		return darken_per_ch((2 * s), d)
+		return darken((2 * s), d)
 	}
-	return lighten_per_ch((2 * (s - mid)), d)
+	return lighten((2 * (s - mid)), d)
 }
 
-// HARD MIX (check)
-func hard_mix(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, hard_mix_per_ch)
+// HardMix ...
+func HardMix(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, hardMix)
 }
-func hard_mix_per_ch(d, s float64) float64 {
-	if vivid_light_per_ch(d, s) < mid {
+func hardMix(d, s float64) float64 {
+	if vividLight(d, s) < mid {
 		return 0.0
 	}
 	return max
@@ -299,46 +298,46 @@ func hard_mix_per_ch(d, s float64) float64 {
 
 /*-------------------------------------------------------*/
 
-// DIFFERENCE
-func difference(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, difference_per_ch)
+// Difference ...
+func Difference(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, difference)
 }
-func difference_per_ch(d, s float64) float64 {
+func difference(d, s float64) float64 {
 	return math.Abs(s - d)
 }
 
-// EXCLUSION
-func exclusion(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, exclusion_per_ch)
+// Exclusion ...
+func Exclusion(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, exclusion)
 }
-func exclusion_per_ch(d, s float64) float64 {
+func exclusion(d, s float64) float64 {
 	return s + d - s*d/mid
 }
 
-// SUBSTRACT
-func substract(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, substract_per_ch)
+// Substract ...
+func Substract(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, substract)
 }
-func substract_per_ch(d, s float64) float64 {
+func substract(d, s float64) float64 {
 	if d-s < 0.0 {
 		return 0.0
 	}
 	return d - s
 }
 
-// DIVIDE
-func divide(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, divide_per_ch)
+// Divide ...
+func Divide(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, divide)
 }
-func divide_per_ch(d, s float64) float64 {
+func divide(d, s float64) float64 {
 	return (d*max)/s + 1.0
 }
 
 // Blending modes that use HSL color model transformations.
 /*-------------------------------------------------------*/
 
-// HUE
-func hue(dst, src color.Color) color.Color {
+// Hue ...
+func Hue(dst, src color.Color) color.Color {
 	s := rgb2hsl(src)
 	if s.s == 0.0 {
 		return dst
@@ -347,22 +346,22 @@ func hue(dst, src color.Color) color.Color {
 	return hsl2rgb(s.h, d.s, d.l)
 }
 
-// SATURATION
-func saturation(dst, src color.Color) color.Color {
+// Saturation ...
+func Saturation(dst, src color.Color) color.Color {
 	s := rgb2hsl(src)
 	d := rgb2hsl(dst)
 	return hsl2rgb(d.h, s.s, d.l)
 }
 
-// COLOR "added _ to avoid namespace conflict with 'color' package"
-func color_(dst, src color.Color) color.Color {
+// Color ...
+func Color(dst, src color.Color) color.Color {
 	s := rgb2hsl(src)
 	d := rgb2hsl(dst)
 	return hsl2rgb(s.h, s.s, d.l)
 }
 
-// LUMINOSITY
-func luminosity(dst, src color.Color) color.Color {
+// Luminosity ...
+func Luminosity(dst, src color.Color) color.Color {
 	s := rgb2hsl(src)
 	d := rgb2hsl(dst)
 	return hsl2rgb(d.h, d.s, s.l)
@@ -372,64 +371,32 @@ func luminosity(dst, src color.Color) color.Color {
 // or GIMP at the moment, but produced their desired results.
 /*-------------------------------------------------------*/
 
-// ADD
-func add(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, add_per_ch)
+// Add ...
+func Add(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, add)
 }
-func add_per_ch(d, s float64) float64 {
+func add(d, s float64) float64 {
 	if s+d > max {
 		return max
 	}
 	return s + d
 }
 
-// REFLEX (a.k.a GLOW)
-func reflex(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, reflex_per_ch)
+// Reflex (a.k.a. Glow)
+func Reflex(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, reflex)
 }
-func reflex_per_ch(d, s float64) float64 {
+func reflex(d, s float64) float64 {
 	if s == max {
 		return s
 	}
 	return math.Min(max, (d * d / (max - s)))
 }
 
-// PHOENIX
-func phoenix(dst, src color.Color) color.Color {
-	return blendPerChannel(dst, src, phoenix_per_ch)
+// Phoenix ...
+func Phoenix(dst, src color.Color) color.Color {
+	return blendPerChannel(dst, src, phoenix)
 }
-func phoenix_per_ch(d, s float64) float64 {
+func phoenix(d, s float64) float64 {
 	return math.Min(d, s) - math.Max(d, s) + max
-}
-
-// Init function maps the blending mode functions.
-func init() {
-	Darken = darken
-	Multiply = multiply
-	ColorBurn = color_burn
-	LinearBurn = linear_burn
-	DarkerColor = darker_color
-	Lighten = lighten
-	Screen = screen
-	ColorDodge = color_dodge
-	LinearDodge = linear_dodge
-	LighterColor = lighter_color
-	Overlay = overlay
-	SoftLight = soft_light
-	HardLight = hard_light
-	VividLight = vivid_light
-	LinearLight = linear_light
-	PinLight = pin_light
-	HardMix = hard_mix
-	Difference = difference
-	Exclusion = exclusion
-	Substract = substract
-	Divide = divide
-	Hue = hue
-	Saturation = saturation
-	Color = color_
-	Luminosity = luminosity
-	Add = add
-	Reflex = reflex
-	Phoenix = phoenix
 }
--- examples/blending/main.go
+++ examples/blending/main.go
@@ -8,44 +8,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/phrozen/blend"
 	"image"
 	"image/jpeg"
 	"os"
+
+	"github.com/phrozen/blend"
 )
 
-var modes = map[string]blend.BlendFunc{
-	"add":           blend.Add,
-	"color":         blend.Color,
-	"color_burn":    blend.ColorBurn,
-	"color_dodge":   blend.ColorDodge,
-	"darken":        blend.Darken,
-	"darker_color":  blend.DarkerColor,
-	"difference":    blend.Difference,
-	"divide":        blend.Divide,
-	"exclusion":     blend.Exclusion,
-	"hard_light":    blend.HardLight,
-	"hard_mix":      blend.HardMix,
-	"hue":           blend.Hue,
-	"lighten":       blend.Lighten,
-	"lighter_color": blend.LighterColor,
-	"linear_burn":   blend.LinearBurn,
-	"linear_dodge":  blend.LinearDodge,
-	"linear_light":  blend.LinearLight,
-	"luminosity":    blend.Luminosity,
-	"multiply":      blend.Multiply,
-	"overlay":       blend.Overlay,
-	"phoenix":       blend.Phoenix,
-	"pin_light":     blend.PinLight,
-	"reflex":        blend.Reflex,
-	"saturation":    blend.Saturation,
-	"screen":        blend.Screen,
-	"soft_light":    blend.SoftLight,
-	"substract":     blend.Substract,
-	"vivid_light":   blend.VividLight,
-}
-
-func LoadJPG(filename string) (image.Image, error) {
+func loadJPG(filename string) (image.Image, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
@@ -59,13 +29,13 @@ func LoadJPG(filename string) (image.Image, error) {
 	return img, nil
 }
 
-func SaveJPG(filename string, img image.Image) error {
+func saveJPG(filename string, img image.Image) error {
 	file, err := os.Create(filename + ".jpg")
 	if err != nil {
 		return err
 	}
 
-	if err := jpeg.Encode(file, img, &jpeg.Options{85}); err != nil {
+	if err := jpeg.Encode(file, img, &jpeg.Options{Quality: 85}); err != nil {
 		return err
 	}
 
@@ -81,22 +51,22 @@ func main() {
 	var err error
 	var img image.Image
 
-	dst, err := LoadJPG("destination.jpg")
+	dst, err := loadJPG("destination.jpg")
 	if err != nil {
 		panic(err)
 	}
 
-	src, err := LoadJPG("source.jpg")
+	src, err := loadJPG("source.jpg")
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("This program tests all the color blending modes in the library.")
 
-	for name, mode := range modes {
+	for name, mode := range blend.Modes {
 		fmt.Println("Blending Mode: ", name)
 		img = blend.BlendNewImage(dst, src, mode)
-		err = SaveJPG("blend_"+name, img)
+		err = saveJPG("output/blend_"+name, img)
 		if err != nil {
 			panic(err)
 		}
